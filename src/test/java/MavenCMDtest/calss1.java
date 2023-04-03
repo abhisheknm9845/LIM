ae2c57a77c71e2b0d7023a7df2ceeaff6aea0d02
johns <johns@Johnsy>
2023-04-03 14:45:14 +0530

    Here i changed smoke suite as a regression suite.

--- src/test/java/MavenCMDtest/calss1.java
+++ src/test/java/MavenCMDtest/calss1.java
@@ -3,12 +3,12 @@ package MavenCMDtest;
 import org.testng.annotations.Test;
 
 public class calss1 {
-	@Test(groups = "smoke")
+	@Test(groups = "regression")
 	public void maven()
 	{
 		System.out.println("maven should be there");
 	}
-	@Test(groups = "smoke")
+	@Test(groups = "regression")
 	public void command()
 	{
 		System.out.println("command should be there");
