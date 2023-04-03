f7a666c9f5536760c74f7c9b63a619bd980d2e38
johns <johns@Johnsy>
2023-04-03 15:04:08 +0530

    This is a branch 2 test script.

---
+++ src/test/java/MavenCMDtest/Sampledata.java
@@ -0,0 +1,17 @@
+package MavenCMDtest;
+
+import org.testng.annotations.Test;
+
+public class Sampledata {
+	@Test
+	public void testScript1()
+	{
+		System.out.println("sample data1");
+	}
+	@Test
+	public void testScript2()
+	{
+		System.out.println("sample data2");
+	}
+
+}
